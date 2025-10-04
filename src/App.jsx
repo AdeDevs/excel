3e12f461c2164f4bb5d364cfc2c05ee761ed531e
AdeDevs <adeyemiakinyemi01@gmail.com>
2025-10-04 23:39:32 +0100

    initialize React app with Vite, add basic routing and navigation components

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Router, Routes } from "react-router-dom"
+import NavBar from "./screens/Nav"
+
+function App() {
+  return (
+    <div className='parent'>
+      <NavBar />
+      <Routes>
+        
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ src/screens/Nav.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react"
+import Logo from "../assets/logo.png"
+
+export default function NavBar() {
+    const [isMenu, setIsMenu] = useState(false)
+    const toggleMenu = () => {
+        setIsMenu(!isMenu)
+    }
+    
+    return (
+        <div className="nav-bar">
+            <nav className="navigation">
+                <a className="logo" href="/"><img src={Logo} alt="Logo" /></a>
+                <ul className="desk-nav">
+                    <li><a href="/">about us</a></li>
+                    <li><a href="/">utme & ssce</a></li>
+                    <li><a href="/">ielts & toefl</a></li>
+                    <li><a href="/">online class</a></li>
+                    <li><a href="/">contact</a></li>
+                    <li><a href="/">blog</a></li>
+                    <li className="tog" onClick={toggleMenu}>Open</li>
+                </ul>
+                <div className={`overlay ${isMenu ? "open" : ""}`} onClick={toggleMenu}></div>
+                <ul className={`mob-nav ${isMenu ? "open" : ""}`}>
+                    <li className="tog" onClick={toggleMenu}>Close</li>
+                    <li onClick={toggleMenu}><a href="/">about us</a></li>
+                    <li onClick={toggleMenu}><a href="/">utme & ssce</a></li>
+                    <li onClick={toggleMenu}><a href="/">ielts & toefl</a></li>
+                    <li onClick={toggleMenu}><a href="/">online class</a></li>
+                    <li onClick={toggleMenu}><a href="/">contact</a></li>
+                    <li onClick={toggleMenu}><a href="/">blog</a></li>
+                </ul>
+            </nav>
+            <div className="msg">
+                <a href="/">Click here now to register for the free online UTME & SSCE class.</a>
+            </div>
+        </div>
+    )
+}
